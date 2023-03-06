25b6a32bd24db5606dc5d11818ac38de9fe89937
warengeorge <georgeomiebi@gmail.com>
2023-03-06 16:23:44 +0100

    Added script2

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Fix and allow user holberton with unlimited limit
+
+exec { 'fix_limit_hbton_user':
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# limit users
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
